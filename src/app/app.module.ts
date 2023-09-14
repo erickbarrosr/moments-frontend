7febecfc78fb3f5316696e71a264f1cb6a793efc
Erick Barros <erickbarrosrezende@gmail.com>
2023-09-14 19:02:31 -0300

    feat: created moment-form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { AboutComponent } from './components/pages/about/about.component';
 import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
+import { MomentFormComponent } from './components/moment-form/moment-form.component';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { NewMomentComponent } from './components/pages/new-moment/new-moment.com
     HomeComponent,
     AboutComponent,
     NewMomentComponent,
+    MomentFormComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
