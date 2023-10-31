54ffc21e1bafbcb419543fe731437f9d5e3aabfd
Erick Barros <erickbarrosrezende@gmail.com>
2023-10-31 19:16:36 -0300

    refactor (new-moment.component.ts): changed property name

--- src/app/components/pages/new-moment/new-moment.component.ts
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./new-moment.component.css'],
 })
 export class NewMomentComponent {
-  shareButton = 'Compartilhar';
+  btnText = 'Compartilhar';
 }
