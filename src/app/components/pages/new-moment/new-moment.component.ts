61cdafb59654d6b01d84ed77dc96b4663d929a78
Erick Barros <erickbarrosrezende@gmail.com>
2023-11-18 23:31:12 -0300

    feat(new-moment.component.ts): append data in moment

--- src/app/components/pages/new-moment/new-moment.component.ts
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+import { MomentService } from 'src/app/services/moment.service';
+
+import { Router } from '@angular/router';
+
+import { MessagesService } from 'src/app/services/messages.service';
+import { Moment } from 'src/app/Moment';
 
 @Component({
   selector: 'app-new-moment',
   templateUrl: './new-moment.component.html',
   styleUrls: ['./new-moment.component.css'],
 })
-export class NewMomentComponent {
+export class NewMomentComponent implements OnInit {
   shareButton = 'Compartilhar';
+  image?: File;
 
-  createHandler(event: any) {}
+  constructor(private momentService: MomentService) {}
+
+  ngOnInit(): void {}
+
+  async createHandler(moment: Moment) {
+    const formData = new FormData();
+
+    formData.append('title', moment.title);
+    formData.append('description', moment.description);
+
+    if (moment.image) {
+      formData.append('image', moment.image);
+    }
+
+    await this.momentService.createMoment(formData).subscribe();
+  }
 }
